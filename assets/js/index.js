0f86d83a6a573cc7681c06751a142c247c31141a
geoffrey45 <geoffreymungai45@gmail.com>
2021-05-11 16:27:19 +0300

    use templates to display cards
    implement new card design

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 const ul=document.getElementById('posts')
-let get_posts_url = 'http://127.0.0.1:5000';
+let get_posts_url = 'https://be-social-be.herokuapp.com/';
 
 function createNode(element){
     return document.createElement(element)
@@ -12,24 +12,22 @@ function append(parent, el){
 const fetchOptions = {
     method: 'GET'
 }
-// document.getElementById('post_card').style.display = "None";
 
 fetch(get_posts_url, fetchOptions)
     .then((resp) => resp.json())
     .then(function (data){
         let posts = data;
         let posts_container = document.getElementById('posts_container');
-        let post_card = document.getElementById('posts_card');
+        let template = document.querySelector('#posts_card');
 
+        console.log('fetching posts')
         return posts.map(function (post){
-            let this_card = post_card.cloneNode(true)
-            document.getElementById('post_body').innerHTML = `${post.post_body}`;
+            let this_card = template.content.cloneNode(true);
+            this_card.getElementById('post_body').innerHTML = `${post.post_body}`;
 
             append(posts_container, this_card)
-            
-            
         })
+
+
     })
     .catch(error => console.log('error', error))
-
-
